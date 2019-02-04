f710efb9a9d4f7ca2b4efd40e890f0a5bd6ebd99
stephenkpotter <stephen.potter@puppet.com>
2019-02-04 14:43:50 -0500

    Create FSBO for Shane testing

---
+++ site/profile/manifests/app/fsbo.pp
@@ -0,0 +1,50 @@
+class profile::app::fsbo  {
+
+  $iis_features = [
+    'Web-Server',
+    'Web-WebServer',
+    'Web-Http-Redirect',
+    'Web-Mgmt-Console',
+    'Web-Mgmt-Tools',
+  ]
+
+iis_feature { $iis_features:
+  ensure => 'present',
+}
+
+file { 'c:\\inetpub\\wwwroot\\famis':
+  ensure => 'directory'
+}
+
+file { 'c:\\inetpub\\wwwroot\\famis\\portal':
+  ensure => 'directory'
+}
+
+    iis_application_pool { 'famis':
+      ensure                  => 'present',
+      state                   => 'started',
+      managed_pipeline_mode   => 'Integrated',
+      managed_runtime_version => 'v4.0',
+      start_mode              => 'OnDemand',
+      identity_type           => 'SpecificUser',
+      user_name               => 'Administrator',
+      password                => $password,
+}
+
+iis_virtual_directory { 'portal':
+     ensure                  => 'present',
+     sitename                => 'Default Web Site\famis',
+     physicalpath            => 'c:\\inetpub\\wwwroot\\famis\\portal',
+     user_name               => 'Administrator',
+     password                => $password,
+     require      =>       File['c:\\inetpub\\wwwroot\\famis\\portal'],
+    }
+
+iis_application {'portal':
+     ensure                   => 'present',
+     virtual_directory        => "IIS:\\Sites\\Default Web Site\\famis\\portal",
+     applicationpool          => 'famis',
+     require                  => File['c:\\inetpub\\wwwroot\\famis\\portal'],
+    }
+
+}
