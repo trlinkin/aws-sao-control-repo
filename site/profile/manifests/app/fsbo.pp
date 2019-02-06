dfd1472fd03e2227521eb5e1f66b0a91c474d7a3
ssorb <shane.sorbello@gmail.com>
2019-02-06 16:08:24 -0500

    Update fsbo.pp

--- site/profile/manifests/app/fsbo.pp
+++ site/profile/manifests/app/fsbo.pp
@@ -1,165 +1,65 @@
- 
+ class profile::app::fsbo {
 
   $iis_features = [
-
     'Web-Server',
-
     'Web-WebServer',
-
     'Web-Http-Redirect',
-
     'Web-Mgmt-Console',
-
     'Web-Mgmt-Tools',
-
   ]
 
- 
-
- 
 
 iis_feature { $iis_features:
-
   ensure => 'present',
-
 }
 
- 
 
 file { 'c:\\inetpub\\wwwroot\\famis':
-
   ensure => 'directory'
-
 }
-
  
-
 file { 'c:\\inetpub\\wwwroot\\famis\\portal':
-
   ensure => 'directory'
-
 }
 
- 
-
-file { 'c:\\inetpub\\wwwroot\\famis\\control-panel':
-
-  ensure => 'directory'
-
-}
-
- 
 
 iis_application_pool { 'famis':
-
       ensure                  => 'present',
-
       state                   => 'started',
-
       managed_pipeline_mode   => 'Integrated',
-
       managed_runtime_version => 'v4.0',
-
       start_mode              => 'OnDemand',
-
       identity_type           => 'SpecificUser',
-
-      user_name               => 'finance360.net\service.devcontent',
-
+      user_name               => 'Administrator',
       password                => 'password',
-
- 
-
 }
-
- 
-
- 
-
- 
-
- 
 
 iis_application { 'Default Web Site\famis/portal':
-
   ensure             => 'present',
-
   applicationpool    => 'famis',
-
   authenticationinfo => {
-
   'iisClientCertificateMapping' => false,
-
   'clientCertificateMapping' => false,
-
   'anonymous' => true,
-
   'basic' => false,
-
   'windows' => false
-
-},
-
+   },
   enabledprotocols   => 'http',
-
   physicalpath       => 'C:\inetpub\wwwroot\famis\portal',
-
-  sitename           => 'Default Web Site',
-
-}
-
- 
-
- 
-
-iis_application { 'Default Web Site\famis/control-panel':
-
-  ensure             => 'present',
-
-  applicationpool    => 'famis',
-
-  authenticationinfo => {
-
-  'iisClientCertificateMapping' => false,
-
-  'clientCertificateMapping' => false,
-
-  'anonymous' => true,
-
-  'basic' => false,
-
-  'windows' => false
-
-},
-
-  enabledprotocols   => 'http',
-
-  physicalpath       => 'C:\inetpub\wwwroot\famis\control-panel',
-
-  sitename           => 'Default Web Site',
-
+  #sitename           => 'Default Web Site',
+  virtual_directory   => "IIS:\\Sites\\Default Web Site\\famis\\portal",
+  require           => Iis_Virtual_Directory['portal'],
 }
 
  
 
 iis_virtual_directory { 'portal':
-
      ensure                  => 'present',
-
      sitename                => 'Default Web Site\famis',
-
      physicalpath            => 'c:\\inetpub\\wwwroot\\famis\\portal',
-
-     user_name               => 'finance360.net\service.devcontent',
-
+     user_name               => 'Administrator',
      password                => 'password',
-
-     require      => Iis_Application['Default Web Site\famis/portal'],
-
-    }
-
- 
-
- 
+    # require      => Iis_Application['Default Web Site\famis/portal'],
+}
 
 }
