92797c44d769e2378f4fa36986174a2a52f7b3e7
ssorb <shane.sorbello@gmail.com>
2019-02-08 10:16:44 -0500

    Update fsbo.pp

--- site/profile/manifests/app/fsbo.pp
+++ site/profile/manifests/app/fsbo.pp
@@ -34,10 +34,10 @@ iis_application_pool { 'famis':
       password                => 'password',
 }
 
-iis_application { 'Default Web Site\famis':
+iis_application { 'Default Web Site\famis/portal':
   ensure             => 'present',
   applicationpool    => 'famis',
-  applicationname    => '/portal',
+  applicationname    => 'portal',
   authenticationinfo => {
   'iisClientCertificateMapping' => false,
   'clientCertificateMapping' => false,
@@ -56,7 +56,7 @@ iis_application { 'Default Web Site\famis':
 
 iis_virtual_directory { 'portal':
      ensure                  => 'present',
-     sitename                => 'Default Web Site\famis/portal',
+     sitename                => 'Default Web Site\famis/',
      #application             => '/portal',
      physicalpath            => 'c:\\inetpub\\wwwroot\\famis\\portal',
      user_name               => 'Administrator',
