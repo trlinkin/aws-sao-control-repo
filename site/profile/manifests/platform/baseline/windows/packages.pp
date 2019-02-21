09432d1ca77a6a99ee8d98fed24cd8093a89666c
ssorb <shane.sorbello@gmail.com>
2019-02-21 14:49:39 -0500

    Update packages.pp
    added a post install reboot for firefox to demonstrate idempotence after reboot

--- site/profile/manifests/platform/baseline/windows/packages.pp
+++ site/profile/manifests/platform/baseline/windows/packages.pp
@@ -9,5 +9,13 @@ class profile::platform::baseline::windows::packages {
   package { '7zip': }
   package { 'git': }
   package { 'uniextract': }
+  package { 'firefox': }
+
+  reboot {'post_install_reboot':
+    subscribe => Package['firefox'],
+    apply     => 'immediately',
+    timeout   => 0,
+  }
+
 
 }
