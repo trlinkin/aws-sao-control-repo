709795dc21f9d7ffc92ee4eadb4e24528c29dcaf
ssorb <shane.sorbello@gmail.com>
2019-02-21 12:25:41 -0500

    Update windows.pp
    added files.pp which creates disables the local configuration manager and creates a file on the desktop using a dsc resource

--- site/profile/manifests/platform/baseline/windows.pp
+++ site/profile/manifests/platform/baseline/windows.pp
@@ -5,6 +5,7 @@ class profile::platform::baseline::windows {
   include ::profile::platform::baseline::windows::motd
   include ::profile::platform::baseline::windows::firewall
   include ::profile::platform::baseline::windows::packages
+  include ::profile::platform::baseline::windows::files
   include ::profile::platform::baseline::users::windows
 
 }
