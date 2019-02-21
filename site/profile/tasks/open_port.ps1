c3257fbb068ee04dab9678fe4762f8e9eab417f5
ssorb <shane.sorbello@gmail.com>
2019-02-21 15:26:18 -0500

    Update open_port.ps1

--- site/profile/tasks/open_port.ps1
+++ site/profile/tasks/open_port.ps1
@@ -1,1 +1,5 @@
-New-NetFirewallRule -DisplayName "$dispname" -Direction Inbound -Action Allow -Protocol "$proto" -LocalPort "$port"
+
+param($dispname)
+param($proto)
+param($port)
+New-NetFirewallRule -DisplayName "$dispname" -Direction Inbound -Action Allow -Protocol $proto -LocalPort $port
